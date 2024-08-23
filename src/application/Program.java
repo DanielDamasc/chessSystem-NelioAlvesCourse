0dd8806fecd2250526b5f0ee4655ecc4620e2e9b
Daniel Damasceno <dandamasceno04@gmail.com>
2024-08-22 23:39:12 -0300

    Printing the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,13 +2,15 @@ package application;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		/* teste */
-		Board board = new Board(8, 8);
+		ChessMatch cm = new ChessMatch();
+		UI.printBoard(cm.getPieces());
 	}
 
 }
